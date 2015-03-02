ffeca4fc163d15ec6acfd108933b16f1c12cf986
Jiashuo Li <jiashuolizz@gmail.com>
2015-03-02 02:19:12 -0800

    Manually merge DEMO "8eb580: minor GUI changes (still ugly)"

---
+++ TempSergey/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using iRobotGUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TempSergey
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowDemoDialog(Instruction ins)
+        {
+            DemoWindow dlg = new DemoWindow();
+            dlg.Owner = Window.GetWindow(this);
+            dlg.Ins = ins;
+            dlg.ShowDialog();
+        }
+
+       
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newIns = new Instruction("DEMO" + " 0");
+            ShowDemoDialog(newIns);
+        }
+    }
+}
---
+++ UI/Controls/DemoParam.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using System.Windows.Forms;
+//using System.String;
+
+namespace iRobotGUI.Controls
+{
+    /// <summary>
+    /// Interaction logic for Demo.xaml
+    /// </summary>
+    public partial class DemoParam : UserControl, IInsParamControl
+    {
+
+        public Instruction _ins;
+
+        public DemoParam()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Handler for selection change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+             var comboBox = sender as ComboBox;
+
+            /// get the number index of selected item
+            int num = comboBox.SelectedIndex;
+            if (num == 10)
+            {
+                num = -1;
+            };
+            CurrentIns.parameters[0] = num;
+           
+        }
+        /// <summary>
+        /// This sets menu item when ComboBox1 loads
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox1_Loaded(object sender, RoutedEventArgs e)
+        {
+            var comboBox = sender as ComboBox;
+            //Make the first item selected:
+            comboBox.SelectedIndex = 0;
+        }
+        private void OnHover(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public Instruction CurrentIns
+        {
+            get
+            {
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+
+                ComboBox1.SelectedIndex = CurrentIns.parameters[0]; /// ComboBox1.FindName(value);
+            }
+        }
+    }
+}
