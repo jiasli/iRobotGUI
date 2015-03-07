0c9c4322f81d28d223cf32619e98283234175204
Jiashuo Li <jiashuolizz@gmail.com>
2015-03-06 19:58:32 -0800

    LedParam: use private method to get/set bit

--- UI/Controls/LedParam.xaml.cs
+++ UI/Controls/LedParam.xaml.cs
@@ -16,18 +16,21 @@ using System.Windows.Shapes;
 
 namespace iRobotGUI.Controls
 {
-    /// <summary>
-    /// Interaction logic for LEDControl.xaml
-    /// </summary>
-    public partial class LedParam : BaseParamControl
-    {
-        Color onColor = Colors.Lime;
-        Color offColor = Colors.Black;
+	/// <summary>
+	/// Interaction logic for LEDControl.xaml
+	/// </summary>
+	public partial class LedParam : BaseParamControl
+	{
+		Color onColor = Colors.Lime;
+		Color offColor = Colors.Black;
 
-        public LedParam()
-        {
-            InitializeComponent();
-        }
+		private const int PLAY_BIT = 1;
+		private const int ADVANCE_BIT = 3;
+
+		public LedParam()
+		{
+			InitializeComponent();
+		}
 
 
 		public override Instruction Ins
@@ -44,72 +47,99 @@ namespace iRobotGUI.Controls
 				SliderIntensity.Value = Ins.parameters[2];
 
 				// Set checkbox status according to 4th and 2nd bits      
-				CheckBoxPlay.IsChecked = (Ins.parameters[0] & 2) > 0;
-				CheckBoxAdvance.IsChecked = (Ins.parameters[0] & 8) > 0;
+				CheckBoxPlay.IsChecked = GetBit(Ins.parameters[0], PLAY_BIT);
+				CheckBoxAdvance.IsChecked = GetBit(Ins.parameters[0], ADVANCE_BIT);
 			}
 		}
 
-        private void CheckBoxPlay_Checked(object sender, RoutedEventArgs e)
-        {
-            // Colors: https://msdn.microsoft.com/en-us/library/system.windows.media.colors(v=vs.110).aspx
-            EllipsePlay.Fill = new SolidColorBrush(onColor);
+		private void CheckBoxPlay_Checked(object sender, RoutedEventArgs e)
+		{
+			// Colors: https://msdn.microsoft.com/en-us/library/system.windows.media.colors(v=vs.110).aspx
+			EllipsePlay.Fill = new SolidColorBrush(onColor);
 
-            Ins.parameters[0] |= 2;
-        }
+			Ins.parameters[0] = SetBit(Ins.parameters[0], PLAY_BIT, true);
+		}
 
-        private void CheckBoxPlay_Unchecked(object sender, RoutedEventArgs e)
-        {
-            EllipsePlay.Fill = new SolidColorBrush(offColor);
-            Ins.parameters[0] &= ~2;
-        }
+		private void CheckBoxPlay_Unchecked(object sender, RoutedEventArgs e)
+		{
+			EllipsePlay.Fill = new SolidColorBrush(offColor);
+			Ins.parameters[0] = SetBit(Ins.parameters[0], PLAY_BIT, false);
+		}
 
-        private void CheckBoxAdvance_Checked(object sender, RoutedEventArgs e)
-        {
-            EllipseAdvance.Fill = new SolidColorBrush(onColor);
-            Ins.parameters[0] |= 8;
-        }
+		private void CheckBoxAdvance_Checked(object sender, RoutedEventArgs e)
+		{
+			EllipseAdvance.Fill = new SolidColorBrush(onColor);
+			Ins.parameters[0] = SetBit(Ins.parameters[0], ADVANCE_BIT, true);
+		}
 
-        private void CheckBoxAdvance_Unchecked(object sender, RoutedEventArgs e)
-        {
-            EllipseAdvance.Fill = new SolidColorBrush(offColor);
-            Ins.parameters[0] &= ~8;
-        }
+		private void CheckBoxAdvance_Unchecked(object sender, RoutedEventArgs e)
+		{
+			EllipseAdvance.Fill = new SolidColorBrush(offColor);
+			Ins.parameters[0] = SetBit(Ins.parameters[0], ADVANCE_BIT, false);
+		}
 
-        // Separate event handler for Sliders, or error will happen at initialization - Original value will be modified with uninitialized Slider value
-        private void SliderColor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            EllipsePower.Fill = new SolidColorBrush(GetLedRgbColor(SliderColor.Value, SliderIntensity.Value));
-            Ins.parameters[1] = (int)e.NewValue;
-        }
+		// Separate event handler for Sliders, or error will happen at initialization - Original value will be modified with uninitialized Slider value
+		private void SliderColor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			EllipsePower.Fill = new SolidColorBrush(GetLedRgbColor(SliderColor.Value, SliderIntensity.Value));
+			Ins.parameters[1] = (int)e.NewValue;
+		}
 
-        private void SliderIntensity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            EllipsePower.Fill = new SolidColorBrush(GetLedRgbColor(SliderColor.Value, SliderIntensity.Value));
-            Ins.parameters[2] = (int)e.NewValue;
-        }
+		private void SliderIntensity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+		{
+			EllipsePower.Fill = new SolidColorBrush(GetLedRgbColor(SliderColor.Value, SliderIntensity.Value));
+			Ins.parameters[2] = (int)e.NewValue;
+		}
 
-        private Color GetLedRgbColor(double color, double intensity)
-        {
-            // 0, 255, 0 Green
-            // 255, 255, 0 Yellow
-            // 255, 0, 0 Red
-            Debug.WriteLine(color + " " + intensity);
-            double intensityRatio = intensity / 255.0;
+		private Color GetLedRgbColor(double color, double intensity)
+		{
+			// 0, 255, 0 Green
+			// 255, 255, 0 Yellow
+			// 255, 0, 0 Red
+			Debug.WriteLine(color + " " + intensity);
+			double intensityRatio = intensity / 255.0;
 
-            Color rgbColor;
-            if (color < 128)
-                rgbColor = Color.FromRgb(
-                    (byte)(color * 2 * intensityRatio),
-                    (byte)(255 * intensityRatio),
-                    0);
-            else rgbColor = Color.FromRgb(
-                (byte)(255 * intensityRatio),
-                (byte)((255 - (color - 128) * 2) * intensityRatio),
-                0);
+			Color rgbColor;
+			if (color < 128)
+				rgbColor = Color.FromRgb(
+					(byte)(color * 2 * intensityRatio),
+					(byte)(255 * intensityRatio),
+					0);
+			else rgbColor = Color.FromRgb(
+				(byte)(255 * intensityRatio),
+				(byte)((255 - (color - 128) * 2) * intensityRatio),
+				0);
 
-            return rgbColor;
-        }
+			return rgbColor;
+		}
 
-      
-    }
+
+		/// <summary>
+		/// Set the specific bit of an integer, starting from the small end.
+		/// </summary>
+		/// <param name="n">The integer being set. </param>
+		/// <param name="index">The index from the small end. </param>
+		/// <param name="value">0 or 1 </param>
+		/// <returns>The integer after modification. </returns>
+		private int SetBit(int n, int index, bool value)
+		{
+			// clear index-th bit.
+			n = n & ~(1 << index);
+			n = n | ((value ? 1 : 0) << index);
+			return n;
+		}
+
+		/// <summary>
+		/// Get the specific bit of an integer, starting from the small end.
+		/// </summary>
+		/// <param name="n">The integer being set. </param>
+		/// <param name="index">The index from the small end. </param>
+		/// <returns></returns>
+		private bool GetBit(int n, int index)
+		{
+			return (n & (1 << index)) > 0;
+		}
+
+
+	}
 }
