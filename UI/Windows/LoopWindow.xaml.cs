d9afba0e38d780f1aba38792e6cc4448ec396b2e
Jiashuo Li <jiashuolizz@gmail.com>
2015-03-08 18:59:13 -0700

    Extract ConditionPanel

--- UI/Windows/LoopWindow.xaml.cs
+++ UI/Windows/LoopWindow.xaml.cs
@@ -21,48 +21,9 @@ namespace iRobotGUI.Windows
 	/// </summary>
 	public partial class LoopWindow : Window
 	{
-		InsCondition condition;
-
 		public LoopWindow()
 		{
-			condition = new InsCondition();
 			InitializeComponent();
-			UpdateConditionWindow();
-		}
-
-		private void ComboBoxSensor_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			condition.sensor = ComboBoxSensor.SelectedIndex;
-			UpdateConditionWindow();
-		}
-
-		private void ComboBoxOperator_SelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			condition.op = ComboBoxOperator.SelectedIndex;
-			UpdateConditionWindow();
-		}
-
-		private void TextBoxNum_TextChanged(object sender, TextChangedEventArgs e)
-		{
-			if (!Int32.TryParse(TextBoxNum.Text, out condition.num))
-			{
-				MessageBox.Show("Only numbers are permitted.");
-			}
-			else
-			{
-				UpdateConditionWindow();
-			}
-		}
-
-		private void UpdateConditionWindow()
-		{
-			if (LabelCondition!=null)
-			{
-				LabelCondition.Content = String.Format("sensor[{0}] {1} {2}",
-				condition.sensor,
-				Operator.GetOperatorTextSymbol(condition.op),
-				condition.num);
-			}		
 		}
 	}
 }
