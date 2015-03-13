fce554349c381f297673c18cc07723c477603807
fmascare <fem0022@gmail.com>
2015-03-12 23:21:05 -0500

    Re-organized unit tests and reduced duplicate code

--- UnitTests/TranslatorTest.cs
+++ UnitTests/TranslatorTest.cs
@@ -13,122 +13,76 @@ namespace iRobotGUI.Tests
     public class TranslatorTest
     {
         [TestMethod()]
-        [TestProperty("Opcode", "FORWARD")]
-        //New code: Trying to create light weight data driven unit test
-        public void TranslateInstructionTestHelper()
+        public void TranslatePrintInstrString(string actualInstr)
         {
-            try
-            {      
-                string op = TestContext.Properties["Opcode"] as string;
-                Console.WriteLine("op = {0}",op);
-                Instruction a = new Instruction(op);
-                TranslateInstructionTest(a);
-            }
-            catch(NullReferenceException)
-            {
-                Console.WriteLine("null reference exception");
-            }            
-        }
-        public TestContext TestContext
-        {
-            get;
-            set;
-        }
-        //End of new code
-        public void TranslateInstructionTest(Instruction instruction)
-        {
-            string validOpCodeSrting = "FORWARD";
-            string invalidOpCodeString = "INFORNTOF";
-            bool pass = true;
-
-            try
-            {
-                if (instruction.opcode.Equals(validOpCodeSrting))
-                {
-                    pass = true;
-                    Console.WriteLine(validOpCodeSrting+": Valid");
-                }
-                else
-                if (instruction.opcode.Equals(invalidOpCodeString))
-                {
-                    pass = false;
-                    Console.WriteLine(invalidOpCodeString+": Invalid");
-                }
-                else
-                {
-                    pass = true;
-                    Console.WriteLine(invalidOpCodeString+": Valid");
-                }
-            }
-            catch (NullReferenceException)
-            {
-                pass = false;
-            }
-            Assert.IsTrue(pass);
-        }
-
-        [TestMethod()]
-        public void TranslateProgramTest()
-        {
-            string navProgram = "FORWARD 10,3\nLED 10,125,125";
-            string Actual_program = Translator.TranslateProgram(new HLProgram(navProgram));
-            Console.WriteLine(Actual_program);
-            Assert.IsNotNull(Actual_program);
-           
-        }
-
-        [TestMethod()]
-        public void TranslateProgramStringTest()
-        {
-            string ProgramString = "FORWARD 50,5\nLED 10,125,125";
-            string Actual_program = Translator.TranslateProgram(new HLProgram(ProgramString));
-            Console.WriteLine(Actual_program);
-            Assert.IsNotNull(Actual_program);
-        }
-
-        [TestMethod()]
-        public void TranslateInstructionStringTest()
-        {
-            string instructionString = "LED 10,125,125";
-            string Actual_Ins= Translator.TranslateInstruction(new Instruction(instructionString));
+            string input = actualInstr.Trim();
+            string Actual_Ins = Translator.TranslateInstruction(new Instruction(input));
             Console.WriteLine(Actual_Ins);
             Assert.IsNotNull(Actual_Ins);
+        }
+
+        public void TranslatePrintProgString(string actualProg)
+        {
+            string input = actualProg.Trim();
+            string Actual_Program = Translator.TranslateProgram(new HLProgram(input));
+            Console.WriteLine(Actual_Program);
+            Assert.IsNotNull(Actual_Program);
+        }
+
+        [TestMethod()]
+        public void TranslateDemo()
+        {
+            string demoInstruction = "DEMO 0";
+            TranslatePrintInstrString(demoInstruction);
         }
 
         [TestMethod()]
         public void TranslateDrive()
         {
             string driveInstruction = "DRIVE 10,20";
-            string Actual_Ins = Translator.TranslateInstruction(new Instruction(driveInstruction));
-            Console.WriteLine(Actual_Ins);
-            Assert.IsNotNull(Actual_Ins);
+            TranslatePrintInstrString(driveInstruction);
         }
 
         [TestMethod()]
-        public void TranslateForwardBackward()
+        public void TranslateForward()
         {
-            string ProgramInstruction = "FORWARD 50,5\nBACKWARD 10,3";
-            string Actual_program = Translator.TranslateProgram(new HLProgram(ProgramInstruction));
-            Console.WriteLine(Actual_program);
-            Assert.IsNotNull(Actual_program);
+            string forwardInstruction = "FORWARD 50,5";
+            TranslatePrintInstrString(forwardInstruction);
         }
 
         [TestMethod()]
-        public void TranslateLeftRight()
+        public void TranslateBackward()
         {
-            string ProgramInstruction = "LEFT 50,5\nRIGHT 10,3";
-            string Actual_Program = Translator.TranslateProgram(new HLProgram(ProgramInstruction));
-            Console.WriteLine(Actual_Program);
-            Assert.IsNotNull(Actual_Program);
+            string backwardInstruction = "BACKWARD 10,3";
+            TranslatePrintInstrString(backwardInstruction);
+        }
+
+        [TestMethod()]
+        public void TranslateLeft()
+        {
+            string leftInstruction = "LEFT 80";
+            TranslatePrintInstrString(leftInstruction);
+        }
+
+        [TestMethod()]
+        public void TranslateRight()
+        {
+            string rightInstruction = "RIGHT 80";
+            TranslatePrintInstrString(rightInstruction);
         }
 
         [TestMethod()]
         public void TranslateLED()
         {
             string ledInstruction = "LED 10,125,125";
-            string Actual_Ins = Translator.TranslateInstruction(new Instruction(ledInstruction));
-            Console.WriteLine(Actual_Ins);
-            Assert.IsNotNull(Actual_Ins);
+            TranslatePrintInstrString(ledInstruction);
+        }
+
+        [TestMethod()]
+        public void TranslateProgram()
+        {
+            string ProgramInstruction = "FORWARD 50,5\nBACKWARD 10,3\nLEFT 80\nRIGHT 80";
+            TranslatePrintProgString(ProgramInstruction);
         }
 
         [TestMethod()]
