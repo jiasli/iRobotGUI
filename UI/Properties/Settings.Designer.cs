ac2c4650904c82ab250288ba8115cbe72f3f79fc
Jiashuo Li <jiashuolizz@gmail.com>
2015-05-12 16:27:10 -0700

    Set "pop up C" default to false

--- UI/Properties/Settings.Designer.cs
+++ UI/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace iRobotGUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool OpenCCode {
             get {
                 return ((bool)(this["OpenCCode"]));
